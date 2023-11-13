4833b719fa39e9c704ddb7f2b9587efa3f02d2ce
zushigstz <125272871+zushigstz@users.noreply.github.com>
2023-11-13 12:09:02 -0300

    f

--- site/sketch.js
+++ site/sketch.js
@@ -2,7 +2,7 @@ const horas=document.getElementById('horas');
 const minutos=document.getElementById('minutos');
 const segundos=document.getElementById('segundos');
 
-const relogio= setInterval(function time()){
+const relogio= setInterval(function time());{
     let dateToday= new Date();
     let hr= dateToday=dateToday.getHours();
     let min=dateToday=dateToday.getMinutes();
