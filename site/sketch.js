66b9d13cdd7d9b2512372408ad3ab9e39a52e4c6
zushigstz <125272871+zushigstz@users.noreply.github.com>
2023-11-20 11:35:27 -0300

    ff

--- site/sketch.js
+++ site/sketch.js
@@ -1,21 +1,21 @@
-const horas=document.getElementById('horas');
-const minutos=document.getElementById('minutos');
-const segundos=document.getElementById('segundos');
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
-const relogio= setInterval(function time());{
-    let dateToday= new Date();
-    let hr= dateToday=dateToday.getHours();
-    let min=dateToday=dateToday.getMinutes();
-    let seg=dateToday=dateToday.getSeconds();
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
 
-    if (hr<10) hr = '0' + hr;
+    if (hr < 10) hr = '0' + hr;
 
-    if (min<10) min = '0' + min;
+    if (min < 10) min = '0' + min;
 
-    if (seg<10) seg = '0' + seg;
+    if (s < 10) s = '0' + s;
 
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
 
-    horas.textContent=hr;
-    minutos.textContent=min;
-    segundos.textContent=seg;
-}
+})
